3aeac2c0cbfb9d55eebbf7fe0701b898e1e8a3a5
jaen003 <jaenfelliniechavarriatijo@gmail.com>
2023-03-01 19:55:19 -0500

    TEST: add pagination and sort tests

---
+++ Tests/Shared/PaginationLimitTest.cs
@@ -0,0 +1,40 @@
+using Src.Core.Shared.Domain.Exceptions;
+using Src.Core.Shared.Domain.Paginations;
+using Xunit;
+
+namespace Tests.Shared;
+
+public class PaginationLimitTest
+{
+    [Theory]
+    [InlineData(29)]
+    [InlineData(30)]
+    public void IsValid(int value)
+    {
+        int exceptionCode = 0;
+        try
+        {
+            PaginationLimit paginationLimit = new(value);
+        }
+        catch (DomainException exception)
+        {
+            exceptionCode = exception.Code;
+        }
+        Assert.Equal(0, exceptionCode);
+    }
+
+    [Fact]
+    public void IsInvalid()
+    {
+        int exceptionCode = 0;
+        try
+        {
+            PaginationLimit valueObject = new(31);
+        }
+        catch (DomainException exception)
+        {
+            exceptionCode = exception.Code;
+        }
+        Assert.Equal(7, exceptionCode);
+    }
+}
---
+++ Tests/Shared/SortingTypeTest.cs
@@ -0,0 +1,53 @@
+using Src.Core.Shared.Domain.Exceptions;
+using Src.Core.Shared.Domain.Paginations;
+using Xunit;
+
+namespace Tests.Shared;
+
+public class SortingTypeTest
+{
+    [Theory]
+    [InlineData("asc")]
+    [InlineData("desc")]
+    [InlineData(null)]
+    public void IsValid(string? value)
+    {
+        int exceptionCode = 0;
+        try
+        {
+            SortingType sortingType = new(value);
+        }
+        catch (DomainException exception)
+        {
+            exceptionCode = exception.Code;
+        }
+        Assert.Equal(0, exceptionCode);
+    }
+
+    [Theory]
+    [InlineData("ASC")]
+    [InlineData("DESC")]
+    [InlineData("des")]
+    public void IsInvalid(string? value)
+    {
+        int exceptionCode = 0;
+        try
+        {
+            SortingType sortingType = new(value);
+        }
+        catch (DomainException exception)
+        {
+            exceptionCode = exception.Code;
+        }
+        Assert.Equal(9, exceptionCode);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("desc")]
+    public void IsDescending(string? value)
+    {
+        SortingType sortingType = new(value);
+        Assert.True(sortingType.IsDescending());
+    }
+}
