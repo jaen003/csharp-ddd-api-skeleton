6322a6bc255843408f7fe615a66c8b47cf2ea265
jaen003 <jaenfelliniechavarriatijo@gmail.com>
2023-10-29 17:08:09 -0500

    TEST: Add tests for entity status

--- Tests/Products/ProductStatusTest.cs
+++ Tests/Products/ProductStatusTest.cs
@@ -1,4 +1,5 @@
 using Src.Core.Products.Domain.ValueObjects;
+using ApplicationException = Src.Core.Shared.Domain.Exceptions.ApplicationException;
 
 namespace Tests.Products;
 
@@ -17,5 +18,39 @@ public class ProductStatusTest
     {
         ProductStatus valueObject = ProductStatus.CreateDeleted();
         Assert.Equal(2, valueObject.Value);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    public void IsValid(short value)
+    {
+        int exceptionCode = 0;
+        try
+        {
+            ProductStatus valueObject = new(value);
+        }
+        catch (ApplicationException exception)
+        {
+            exceptionCode = exception.Code;
+        }
+        Assert.Equal(0, exceptionCode);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(3)]
+    public void IsInvalid(short value)
+    {
+        int exceptionCode = 0;
+        try
+        {
+            ProductStatus valueObject = new(value);
+        }
+        catch (ApplicationException exception)
+        {
+            exceptionCode = exception.Code;
+        }
+        Assert.Equal(203, exceptionCode);
     }
 }
--- Tests/Resturants/RestaurantStatusTest.cs
+++ Tests/Resturants/RestaurantStatusTest.cs
@@ -1,4 +1,5 @@
 using Src.Core.Restaurants.Domain.ValueObjects;
+using ApplicationException = Src.Core.Shared.Domain.Exceptions.ApplicationException;
 
 namespace Tests.Resturants;
 
@@ -17,5 +18,39 @@ public class RestaurantStatusTest
     {
         RestaurantStatus valueObject = RestaurantStatus.CreateDeleted();
         Assert.Equal(2, valueObject.Value);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    public void IsValid(short value)
+    {
+        int exceptionCode = 0;
+        try
+        {
+            RestaurantStatus valueObject = new(value);
+        }
+        catch (ApplicationException exception)
+        {
+            exceptionCode = exception.Code;
+        }
+        Assert.Equal(0, exceptionCode);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(3)]
+    public void IsInvalid(short value)
+    {
+        int exceptionCode = 0;
+        try
+        {
+            RestaurantStatus valueObject = new(value);
+        }
+        catch (ApplicationException exception)
+        {
+            exceptionCode = exception.Code;
+        }
+        Assert.Equal(102, exceptionCode);
     }
 }
